2f6c7dfcea714a3e8bdab2df127c3002f761c2a4
Maxime Fabre <ehtnam6@gmail.com>
2014-09-23 10:48:14 +0200

    Update Travis file

--- examples/MediaObject.php
+++ examples/MediaObject.php
@@ -5,7 +5,6 @@ use HtmlObject\Traits\Tag;
 
 /**
  * MediaObject::create('image.jpg', 'John Doe', 'My name is John Doe')
- *
  * <article class="media">
  *   <figure class="media-object">
  *     <img src="image.jpg">
@@ -22,44 +21,44 @@ use HtmlObject\Traits\Tag;
  */
 class MediaObject extends Tag
 {
-  /**
-   * The Media Object's tag
-   *
-   * @var string
-   */
-  protected $element = 'article';
+	/**
+	 * The Media Object's tag
+	 *
+	 * @var string
+	 */
+	protected $element = 'article';
 
-  /**
-   * Build a new Media Object
-   *
-   * @param string $image   Image URL
-   * @param string $title   Title
-   * @param string $content Content
-   */
-  public function __construct($image, $title, $content)
-  {
-    $this->addClass('media');
+	/**
+	 * Build a new Media Object
+	 *
+	 * @param string $image   Image URL
+	 * @param string $title   Title
+	 * @param string $content Content
+	 */
+	public function __construct($image, $title, $content)
+	{
+		$this->addClass('media');
 
-    $image  = Image::create($image);
-    $figure = Element::figure($image)->class('media-object');
+		$image  = Image::create($image);
+		$figure = Element::figure($image)->class('media-object');
 
-    $body   = Element::div()->class('media-body');
-    $title  = Element::h2($title)->class('media-heading');
+		$body  = Element::div()->class('media-body');
+		$title = Element::h2($title)->class('media-heading');
 
-    $this->nest(array(
-      'figure' => $figure,
-      'body'   => $body->nest(array(
-        'title'   => $title,
-        'content' => $content,
-      )),
-    ));
-  }
+		$this->nest(array(
+			'figure' => $figure,
+			'body'   => $body->nest(array(
+				'title'   => $title,
+				'content' => $content,
+			)),
+		));
+	}
 
-  /**
-   * Static alias for constructor
-   */
-  public static function create($image, $title, $content)
-  {
-    return new static($image, $title, $content);
-  }
+	/**
+	 * Static alias for constructor
+	 */
+	public static function create($image, $title, $content)
+	{
+		return new static($image, $title, $content);
+	}
 }
